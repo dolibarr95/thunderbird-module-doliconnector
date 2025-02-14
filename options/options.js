dd5c1d38a80dafa639e5c825d09fe830e14286cf
dolibarr95 <24292300+dolibarr95@users.noreply.github.com>
2025-02-14 11:06:23 +0100

    Update options.js
    https://github.com/THERSANE-DOLI/thunderbird-module-doliconnector/issues/5

--- options/options.js
+++ options/options.js
@@ -17,7 +17,12 @@ function restoreOptions() {
     function setCurrentChoice(data) {
         document.getElementById("dolibarr-api-key").value = data.dolibarrApiKey;
         document.getElementById("dolibarr-api-url").value = data.dolibarrApiUrl;
-        document.getElementById("dolibarr-draft-cancel").checked= data.dolibarrDraftCancel;
+        document.getElementById("dolibarr-propal-canceled").checked = data.dolibarrPropalCanceled;
+		document.getElementById("dolibarr-propal-draft").checked = data.dolibarrPropalDraft;
+		document.getElementById("dolibarr-propal-validated").checked = data.dolibarrPropalValidated;
+		document.getElementById("dolibarr-propal-signed").checked = data.dolibarrPropalSigned;
+		document.getElementById("dolibarr-propal-notsigned").checked = data.dolibarrPropalNotSigned;
+		document.getElementById("dolibarr-propal-billed").checked = data.dolibarrPropalBilled;
     }
 
 
@@ -25,7 +30,13 @@ function restoreOptions() {
     document.title = browser.i18n.getMessage("extensionName") + " " + browser.i18n.getMessage("options.options");
     document.getElementById("label-for-dolibarr-api-url").textContent = browser.i18n.getMessage("dolibarrUrl");
     document.getElementById("label-for-dolibarr-api-key").textContent = browser.i18n.getMessage("dolibarrApiKey");
-    document.getElementById("label-for-dolibarr-draft-cancel").textContent = browser.i18n.getMessage("dolibarrDraftCancel");
+    document.getElementById("dolibarr-propal").textContent = browser.i18n.getMessage("dolibarrPropal");
+	document.getElementById("label-for-dolibarr-propal-canceled").textContent = browser.i18n.getMessage("dolibarrCanceled");
+	document.getElementById("label-for-dolibarr-propal-draft").textContent = browser.i18n.getMessage("dolibarrDraft");
+	document.getElementById("label-for-dolibarr-propal-validated").textContent = browser.i18n.getMessage("dolibarrValidated");
+	document.getElementById("label-for-dolibarr-propal-signed").textContent = browser.i18n.getMessage("dolibarrSigned");
+	document.getElementById("label-for-dolibarr-propal-notsigned").textContent = browser.i18n.getMessage("dolibarrNotSigned");
+	document.getElementById("label-for-dolibarr-propal-billed").textContent = browser.i18n.getMessage("dolibarrBilled");
     document.getElementById("save-dolibarr-options").textContent = browser.i18n.getMessage("Save");
 
 
@@ -45,6 +56,11 @@ function saveOptions(e) {
     browser.storage.local.set({
         dolibarrApiKey: document.getElementById("dolibarr-api-key").value,
         dolibarrApiUrl: document.getElementById("dolibarr-api-url").value,
-        dolibarrDraftCancel: document.getElementById("dolibarr-draft-cancel").checked
+        dolibarrPropalCanceled: document.getElementById("dolibarr-propal-canceled").checked,
+		dolibarrPropalDraft: document.getElementById("dolibarr-propal-draft").checked,
+		dolibarrPropalValidated: document.getElementById("dolibarr-propal-validated").checked,
+		dolibarrPropalSigned: document.getElementById("dolibarr-propal-signed").checked,
+		dolibarrPropalNotSigned: document.getElementById("dolibarr-propal-notsigned").checked,
+		dolibarrPropalBilled: document.getElementById("dolibarr-propal-billed").checked
     });
 }
